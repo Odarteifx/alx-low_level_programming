96a6167e72ca17e07a112cea7e6f5c88258c6b51
William Lamptey <williamlamp512@gmail.com>
2022-10-19 05:24:48 -0700

    isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _isalpha - hecks for alphabetic character
+ * @c: a character
+ * Return: 1 or 0 based on conditions
+ */
+int _isalpha(int c)
+{
+	return ((c => 'a' && c <= 'z') || (c => 'A' && c <= 'Z'));
+}
