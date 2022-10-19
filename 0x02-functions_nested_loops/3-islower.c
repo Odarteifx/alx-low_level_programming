f3d0144c10673af9868f83f5bb13ec26c89c6599
William Lamptey <williamlamp512@gmail.com>
2022-10-19 04:46:59 -0700

    Hotfix 3gain

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
- * _islower -> checks for lowercase character
- * Return: 1 if c is lowercase
- * Return: 0 otherwise
- */
+* _islower -> checks for lowercase character
+* @c: character
+* Return: 1 or 0 depending on condition
+*/
 int _islower(int c)
 {
 	return (c >= 'a' && c <= 'z');
