bf5ec355e0ae8097a5a967027414ee5b4ca0da05
William Lamptey <williamlamp512@gmail.com>
2022-10-19 01:39:27 -0700

    10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 -> prints the lower case alphabets 10 times
+ *
+ */
+void print_alphabet_x10(void)
+{
+	int x;
+
+	for (x = '0'; x <= '10'; x++)
+	{
+		int j;
+
+		for (j = 'a'; j <= 'z'; j++)
+		{
+			_putchar(j);
+		}
+		_putchar('\n');
+	}
+}
+
