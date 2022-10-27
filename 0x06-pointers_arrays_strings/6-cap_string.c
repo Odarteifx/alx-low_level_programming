1d5862ae8ab8740e902f40f0bfbb40a35ee83014
William Lamptey <williamlamp512@gmail.com>
2022-10-27 03:17:33 -0700

    hf

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -12,8 +12,8 @@ char *cap_string(char *str)
 	{
 		while (!(str[i] >= 'a' && str[i] <= 'z'))
 			i++;
-		if (str[i - 1] == ' ' ||
-				str[i - 1] == '\t' ||
+		if (str[i - 1] == 32 ||
+				str[i - 1] == 9 ||
 				str[i - 1] == '\n' ||
 				str[i - 1] == ',' ||
 				str[i - 1] == ';' ||
