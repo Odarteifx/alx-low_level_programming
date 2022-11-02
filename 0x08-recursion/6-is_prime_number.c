b4fb244b367c463f1d79ef71e99e343dcf8ab5a4
William Lamptey <williamlamp512@gmail.com>
2022-11-02 16:47:50 -0700

    hotfix

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -2,6 +2,17 @@
 
 int is_divisible(int num, int div);
 int is_prime_number(int n);
+
+/**
+ * is_prime_number - prime identifier
+ * @n: is number
+ * Return: integer
+ */
+int is_prime_number(int n);
+{
+	return (is_divisible(n, 1));
+}
+
 /**
  * is_divisible - Checks if a number is divisible
  * @num: The number to be checked
